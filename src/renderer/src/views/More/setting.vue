cbb057105322effdefe1f9a947f3835adf60344d
hellohyb <1713296741@qq.com>
2024-11-30 18:17:03 +0800

    update

--- src/renderer/src/views/More/setting.vue
+++ src/renderer/src/views/More/setting.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 import { onMounted, reactive, ref, watch } from 'vue'
-import { Edit, Folder,InfoFilled } from '@element-plus/icons-vue'
+import { Edit, Folder, InfoFilled } from '@element-plus/icons-vue'
 const ipcRenderer = window.electron.ipcRenderer
 const electronStore = window.electronStore
-const oldConfig = electronStore.get("config")
+const oldConfig = electronStore.get('config')
 const config = reactive(oldConfig)
 // 修改下载文件夹
 const selectDir = async () => {
@@ -16,10 +16,10 @@ const openCacheDir = () => {
   ipcRenderer.invoke('openDir')
 }
 const selectLocalPlayerDir = async () => {
-    const DirPath = await ipcRenderer.invoke('selectDir')
-    if (DirPath) {
-        config.player.localPath = DirPath
-    }
+  const DirPath = await ipcRenderer.invoke('selectDir')
+  if (DirPath) {
+    config.player.localPath = DirPath
+  }
 }
 // 保存修改
 const saveSetting = () => {
@@ -28,16 +28,23 @@ const saveSetting = () => {
 let favoriteList = ref()
 // 获取收藏夹信息，过滤掉为空的收藏夹
 const getFavoriteList = () => {
-    favoriteList.value = electronStore.get("favorite").filter((item) => {
-      return item.imgList.length != 0
-    })
+  favoriteList.value = electronStore.get('favorite').filter((item) => {
+    return item.imgList.length != 0
+  })
 }
-watch(config,(_new,_old) => {
+watch(
+  config,
+  (_new, _old) => {
     saveSetting()
     // 深拷贝
     const configStr = JSON.parse(JSON.stringify(config))
-    ipcRenderer.send('send-config',{config:configStr,favoriteList:JSON.parse(JSON.stringify(favoriteList.value))})
-},{deep:true})
+    ipcRenderer.send('send-config', {
+      config: configStr,
+      favoriteList: JSON.parse(JSON.stringify(favoriteList.value))
+    })
+  },
+  { deep: true }
+)
 onMounted(async () => {
   getFavoriteList()
 })
@@ -64,35 +71,37 @@ onMounted(async () => {
         <el-radio-group v-model="config.player.no">
           <el-radio :value="1">壁纸库</el-radio>
           <el-radio :value="2">
-            <span style="margin-right: 5px;">我的收藏</span>
+            <span style="margin-right: 5px">我的收藏</span>
             <el-select
-            v-model="config.player.favoriteName"
-            placeholder="选择收藏夹"
-            size="small"
-            style="width: 100px"
-            @click="config.player.no = 2"
+              v-model="config.player.favoriteName"
+              placeholder="选择收藏夹"
+              size="small"
+              style="width: 100px"
+              @click="config.player.no = 2"
             >
-                <el-option
-                    v-for="item in favoriteList"
-                    :key="item.categoryName"
-                    :label="item.categoryName"
-                    :value="item.categoryName"
-                />
+              <el-option
+                v-for="item in favoriteList"
+                :key="item.categoryName"
+                :label="item.categoryName"
+                :value="item.categoryName"
+              />
             </el-select>
-            <el-tooltip
-            class="box-item"
-            effect="dark"
-            content="不会包含空收藏夹"
-            placement="top"
-          >
-          <el-icon><InfoFilled /></el-icon>
-          </el-tooltip>
-        </el-radio>
+            <el-tooltip class="box-item" effect="dark" content="不会包含空收藏夹" placement="top">
+              <el-icon><InfoFilled /></el-icon>
+            </el-tooltip>
+          </el-radio>
           <el-radio :value="3">
             <span>本地目录:</span>
-            <span style="margin:0 2px;font-size: 12px;">{{ config.player.localPath === '' ? "请先选择目录" : config.player.localPath}}</span>
-            <el-button type="primary" size="small" :icon="Edit" @click="selectLocalPlayerDir()"></el-button>
-            </el-radio>
+            <span style="margin: 0 2px; font-size: 12px">{{
+              config.player.localPath === '' ? '请先选择目录' : config.player.localPath
+            }}</span>
+            <el-button
+              type="primary"
+              size="small"
+              :icon="Edit"
+              @click="selectLocalPlayerDir()"
+            ></el-button>
+          </el-radio>
         </el-radio-group>
       </div>
       <div class="items">
@@ -101,19 +110,21 @@ onMounted(async () => {
           <el-radio :value="1" border>按顺序播放</el-radio>
           <el-radio :value="0" border>随机播放</el-radio>
         </el-radio-group>
-        <span v-else style="color: teal;">壁纸库默认随机播放</span>
+        <span v-else style="color: teal">壁纸库默认随机播放</span>
       </div>
       <div class="items">
         <span>壁纸切换间隔：</span>
         <el-input-number v-model="config.player.changeTime" :min="1" :max="100">
-        <template #suffix>
+          <template #suffix>
             <span>分钟</span>
-        </template>
+          </template>
         </el-input-number>
       </div>
-      <span style="font-size: 12px; color: teal">当目录设置为本地目录时，请确保文件夹里面图片文件不为空。设置完成时请在菜单栏重新开启播放壁纸。</span>
+      <span style="font-size: 12px; color: teal"
+        >当目录设置为本地目录时，请确保文件夹里面图片文件不为空。设置完成时请在菜单栏重新开启播放壁纸。</span
+      >
     </div>
-    <div class="set-item">
+    <!-- <div class="set-item">
       <h3 class="title">应用设置</h3>
       <div class="items">
         <span style="margin-right: 5px">开机启动</span>
@@ -130,7 +141,7 @@ onMounted(async () => {
         <br />
         <span style="font-size: 12px; color: teal">动态壁纸运行时关闭窗口默认为最小化，如需退出应用可在任务栏图标右键菜单退出</span>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -159,14 +170,14 @@ onMounted(async () => {
       font-size: 14px;
       width: 100%;
       margin: 10px auto;
-      &:deep(.el-radio__label){
+      &:deep(.el-radio__label) {
         display: flex;
         align-items: center;
       }
-      &:deep(.el-input-number__decrease){
+      &:deep(.el-input-number__decrease) {
         background-color: transparent;
       }
-      &:deep(.el-input-number__increase){
+      &:deep(.el-input-number__increase) {
         background-color: transparent;
       }
     }
